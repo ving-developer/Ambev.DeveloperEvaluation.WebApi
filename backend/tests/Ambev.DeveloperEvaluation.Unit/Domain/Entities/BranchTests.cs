c3efed37a3bb7107294f4b18cd61d3fa31a285db
ving-developer <vingdeveloper@gmail.com>
2025-12-04 11:11:00 -0300

    chore: includding Domain unit tests for Product

---
+++ backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/BranchTests.cs
@@ -0,0 +1,69 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Unit.Domain.Entities.TestData;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Entities;
+
+public class BranchTests
+{
+    [Fact(DisplayName = "Given valid data, When creating branch, Then properties are set")]
+    public void Given_ValidData_When_CreatingBranch_Then_PropertiesSet()
+    {
+        var branch = BranchTestData.GenerateValidBranch();
+
+        Assert.False(string.IsNullOrWhiteSpace(branch.Name));
+        Assert.False(string.IsNullOrWhiteSpace(branch.Code));
+        Assert.False(string.IsNullOrWhiteSpace(branch.City));
+        Assert.False(string.IsNullOrWhiteSpace(branch.State));
+        Assert.NotNull(branch.Sales);
+        Assert.Empty(branch.Sales);
+    }
+
+    [Fact(DisplayName = "Given null name, When creating branch, Then throws ArgumentNullException")]
+    public void Given_NullName_When_CreatingBranch_Then_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Branch(null!, "CODE01", "City", "ST"));
+    }
+
+    [Fact(DisplayName = "Given null code, When creating branch, Then throws ArgumentNullException")]
+    public void Given_NullCode_When_CreatingBranch_Then_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Branch("BranchName", null!, "City", "ST"));
+    }
+
+    [Fact(DisplayName = "Given null city, When creating branch, Then throws ArgumentNullException")]
+    public void Given_NullCity_When_CreatingBranch_Then_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Branch("BranchName", "CODE01", null!, "ST"));
+    }
+
+    [Fact(DisplayName = "Given null state, When creating branch, Then throws ArgumentNullException")]
+    public void Given_NullState_When_CreatingBranch_Then_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Branch("BranchName", "CODE01", "City", null!));
+    }
+
+    [Fact(DisplayName = "Given branch, When adding carts to internal list, Then sales collection updated")]
+    public void Given_Branch_When_AddingSales_Then_SalesCollectionUpdated()
+    {
+        // Guiven
+        var branch = BranchTestData.GenerateValidBranch();
+        var branchId = Guid.NewGuid();
+        typeof(Branch).GetProperty("Id")!.SetValue(branch, branchId);
+
+        var cart1 = new Cart(Guid.NewGuid(), branchId, "SALE001");
+        var cart2 = new Cart(Guid.NewGuid(), branchId, "SALE002");
+        var salesField = typeof(Branch).GetField("_sales", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var salesList = (System.Collections.IList)salesField!.GetValue(branch)!;
+
+        // When
+        salesList.Add(cart1);
+        salesList.Add(cart2);
+
+        // Then
+        Assert.Equal(2, branch.Sales.Count);
+        Assert.Contains(cart1, branch.Sales);
+        Assert.Contains(cart2, branch.Sales);
+    }
+
+}
---
+++ backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/ProductTests.cs
@@ -0,0 +1,65 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using FluentAssertions;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Entities;
+
+/// <summary>
+/// Unit tests for the Product entity.
+/// </summary>
+public class ProductTests
+{
+    [Fact(DisplayName = "Given a new Product, when instantiated, then properties should be initialized correctly")]
+    public void Given_NewProduct_When_Instantiated_Then_PropertiesShouldBeInitialized()
+    {
+        // Given & When
+        var product = new Product();
+
+        // Then
+        product.Title.Should().BeEmpty();
+        product.Description.Should().BeEmpty();
+        product.Image.Should().BeEmpty();
+        product.Price.Should().Be(0);
+        product.Category.Should().Be(default(ProductCategory));
+        product.Rating.Should().BeNull();
+        product.CreatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+        product.UpdatedAt.Should().BeNull();
+    }
+
+    [Fact(DisplayName = "Given a Product, when setting Rating, then it should assign correctly")]
+    public void Given_Product_When_SettingRating_Then_RatingShouldBeAssigned()
+    {
+        // Given
+        var product = new Product();
+        var rating = new Rating(4.5m, 10);
+
+        // When
+        var ratingProperty = typeof(Product).GetProperty("Rating");
+        ratingProperty!.SetValue(product, rating);
+
+        // Then
+        product.Rating.Should().NotBeNull();
+        product.Rating.Should().Be(rating);
+    }
+
+    [Theory(DisplayName = "Given various titles and descriptions, when Product is instantiated, then strings should match")]
+    [InlineData("Product 1", "Description 1")]
+    [InlineData("Product 2", "Description 2")]
+    [InlineData("", "")]
+    public void Given_TitlesAndDescriptions_When_SettingProperties_Then_ShouldMatch(string title, string description)
+    {
+        // Given
+        var product = new Product();
+
+        // When
+        var titleProp = typeof(Product).GetProperty("Title");
+        var descProp = typeof(Product).GetProperty("Description");
+        titleProp!.SetValue(product, title);
+        descProp!.SetValue(product, description);
+
+        // Then
+        product.Title.Should().Be(title);
+        product.Description.Should().Be(description);
+    }
+}
