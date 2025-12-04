644a44a20d1f4e335ed0a4ec1d782f126360bae7
ving-developer <vingdeveloper@gmail.com>
2025-12-04 17:55:28 -0300

    feat: imeplementing SaleModified event

---
+++ backend/src/Ambev.DeveloperEvaluation.Application/Events/Carts/SaleModified/SaleModifiedNotification.cs
@@ -0,0 +1,14 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Events;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Events.Carts.SaleModified;
+
+public class SaleModifiedNotification : INotification
+{
+    public SaleModifiedEvent Event { get; }
+
+    public SaleModifiedNotification(SaleModifiedEvent @event)
+    {
+        Event = @event;
+    }
+}
---
+++ backend/src/Ambev.DeveloperEvaluation.Domain/Events/SaleModifiedEvent.cs
@@ -0,0 +1,13 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Events;
+
+public class SaleModifiedEvent
+{
+    public Cart Cart { get; }
+
+    public SaleModifiedEvent(Cart cart)
+    {
+        Cart = cart;
+    }
+}
