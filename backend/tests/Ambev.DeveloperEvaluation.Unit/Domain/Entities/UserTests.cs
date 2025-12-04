58fbdcdabf2c99510e07753b152b8d43718e7ace
ving-developer <vingdeveloper@gmail.com>
2025-12-04 11:10:17 -0300

    chore: includding Domain tests for Rating

--- backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/UserTests.cs
+++ backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/UserTests.cs
@@ -1,47 +1,150 @@
-using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Domain.Exceptions;
 using Ambev.DeveloperEvaluation.Unit.Domain.Entities.TestData;
+using FluentAssertions;
 using Xunit;
 
 namespace Ambev.DeveloperEvaluation.Unit.Domain.Entities;
 
 /// <summary>
-/// Contains unit tests for the User entity class.
-/// Tests cover status changes and validation scenarios.
+/// Unit tests for the User entity.
+/// Covers creation, validation, and status change scenarios.
 /// </summary>
 public class UserTests
 {
-    /// <summary>
-    /// Tests that when a suspended user is activated, their status changes to Active.
-    /// </summary>
+    #region Creation Tests
+
+    [Fact(DisplayName = "Creating user with valid data should succeed")]
+    public void Given_ValidData_When_CreatingUser_Then_ShouldCreateSuccessfully()
+    {
+        // Guiven
+        var user = UserTestData.GenerateValidUser();
+
+        // When & Then
+        user.Username.Should().NotBeNullOrWhiteSpace();
+        user.Email.Should().Contain("@");
+        user.Phone.Should().MatchRegex(@"^\+?\d{10,15}$");
+        user.Role.Should().NotBe(UserRole.None);
+        user.Status.Should().NotBe(UserStatus.Unknown);
+    }
+
+    #endregion
+
+    #region Username Tests
+
+    [Theory(DisplayName = "SetUsername should throw DomainException for invalid usernames")]
+    [InlineData("")]
+    [InlineData("ab")]
+    [InlineData("thisusernameiswaytoolongtobevalidandshouldfailthisusernameiswaytoolongtobevalidandshouldfail")]
+    [InlineData("invalid username!")]
+    public void Given_InvalidUsername_When_SetUsername_Then_ShouldThrow(string invalidUsername)
+    {
+        var user = UserTestData.GenerateValidUser();
+
+        Action act = () => user.SetUsername(invalidUsername);
+
+        act.Should().Throw<DomainException>()
+            .WithMessage("*Username*");
+    }
+
+    #endregion
+
+    #region Email Tests
+
+    [Theory(DisplayName = "SetEmail should throw DomainException for invalid emails")]
+    [InlineData("")]
+    [InlineData("not-an-email")]
+    [InlineData("invalid@")]
+    public void Given_InvalidEmail_When_SetEmail_Then_ShouldThrow(string invalidEmail)
+    {
+        var user = UserTestData.GenerateValidUser();
+
+        Action act = () => user.SetEmail(invalidEmail);
+
+        act.Should().Throw<DomainException>()
+            .WithMessage("*Email*");
+    }
+
+    #endregion
+
+    #region Phone Tests
+
+    [Theory(DisplayName = "SetPhone should throw DomainException for invalid phones")]
+    [InlineData("")]
+    [InlineData("12345")]
+    [InlineData("+55119")]
+    public void Given_InvalidPhone_When_SetPhone_Then_ShouldThrow(string invalidPhone)
+    {
+        var user = UserTestData.GenerateValidUser();
+
+        Action act = () => user.SetPhone(invalidPhone);
+
+        act.Should().Throw<DomainException>()
+            .WithMessage("*Phone*");
+    }
+
+    #endregion
+
+    #region Role & Status Tests
+
+    [Fact(DisplayName = "SetRole should throw DomainException for invalid role")]
+    public void Given_InvalidRole_When_SetRole_Then_ShouldThrow()
+    {
+        var user = UserTestData.GenerateValidUser();
+
+        Action act = () => user.SetRole((UserRole)999);
+
+        act.Should().Throw<DomainException>()
+            .WithMessage("*role*");
+    }
+
+    [Fact(DisplayName = "SetStatus should throw DomainException for invalid status")]
+    public void Given_InvalidStatus_When_SetStatus_Then_ShouldThrow()
+    {
+        var user = UserTestData.GenerateValidUser();
+
+        Action act = () => user.SetStatus((UserStatus)999);
+
+        act.Should().Throw<DomainException>()
+            .WithMessage("*status*");
+    }
+
+    #endregion
+
+    #region Status Change Methods
+
     [Fact(DisplayName = "User status should change to Active when activated")]
     public void Given_SuspendedUser_When_Activated_Then_StatusShouldBeActive()
     {
-        // Arrange
         var user = UserTestData.GenerateValidUser();
         user.SetStatus(UserStatus.Suspended);
 
-        // Act
         user.Activate();
 
-        // Assert
-        Assert.Equal(UserStatus.Active, user.Status);
+        user.Status.Should().Be(UserStatus.Active);
     }
 
-    /// <summary>
-    /// Tests that when an active user is suspended, their status changes to Suspended.
-    /// </summary>
-    [Fact(DisplayName = "User status should change to Suspended when suspended")]
-    public void Given_ActiveUser_When_Suspended_Then_StatusShouldBeSuspended()
+    [Fact(DisplayName = "User status should change to Inactive when deactivated")]
+    public void Given_ActiveUser_When_Deactivated_Then_StatusShouldBeInactive()
     {
-        // Arrange
         var user = UserTestData.GenerateValidUser();
         user.SetStatus(UserStatus.Active);
 
-        // Act
+        user.Deactivate();
+
+        user.Status.Should().Be(UserStatus.Inactive);
+    }
+
+    [Fact(DisplayName = "User status should change to Suspended when suspended")]
+    public void Given_ActiveUser_When_Suspended_Then_StatusShouldBeSuspended()
+    {
+        var user = UserTestData.GenerateValidUser();
+        user.SetStatus(UserStatus.Active);
+
         user.Suspend();
 
-        // Assert
-        Assert.Equal(UserStatus.Suspended, user.Status);
+        user.Status.Should().Be(UserStatus.Suspended);
     }
+
+    #endregion
 }
---
+++ backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Exceptions/DomainExceptionTests.cs
@@ -0,0 +1,37 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Exceptions;
+
+public class DomainExceptionTests
+{
+    [Fact(DisplayName = "Given message, when creating DomainException, then message is set")]
+    public void Given_Message_When_CreatingDomainException_Then_MessageIsSet()
+    {
+        // Given
+        var message = "Test domain exception";
+
+        // When
+        var exception = new DomainException(message);
+
+        // Then
+        exception.Message.Should().Be(message);
+        exception.InnerException.Should().BeNull();
+    }
+
+    [Fact(DisplayName = "Given message and inner exception, when creating DomainException, then properties are set")]
+    public void Given_MessageAndInnerException_When_CreatingDomainException_Then_PropertiesAreSet()
+    {
+        // Given
+        var message = "Test domain exception";
+        var inner = new InvalidOperationException("Inner exception");
+
+        // When
+        var exception = new DomainException(message, inner);
+
+        // Then
+        exception.Message.Should().Be(message);
+        exception.InnerException.Should().Be(inner);
+    }
+}
